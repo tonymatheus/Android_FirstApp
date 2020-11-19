01f432476ad78862e244e3f6ab256c69f12497a3
tonymatheus <tony.chaves14@gmail.com>
2020-11-19 01:48:36 -0300

    Cria App  e adiciona funcionalidades  simples  ao  layout

---
+++ src/main/java/com/upis/dev/appfirst/MainActivity.java
@@ -0,0 +1,43 @@
+package com.upis.dev.appfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonLimpar;
+    private Button buttonConfirmar;
+    private EditText editText;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonLimpar = (Button) findViewById(R.id.buttonLimpar);
+        buttonConfirmar = (Button) findViewById(R.id.buttonConfirmar);
+        editText = (EditText) findViewById(R.id.edittext);
+        textView = (TextView) findViewById(R.id.textView);
+
+
+
+        buttonLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+                textView.setText(getText(R.string.text_padrao));
+            }
+        });
+
+    }
+    public void confirmar(View view){
+        textView.setText(editText.getText());
+    }
+}
